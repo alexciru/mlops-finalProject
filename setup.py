0083107be3e396054425bd8a5692b631cf9a7533
Alexciru <alejandro.cirugeda@estudiante.uam.esm>
2023-01-06 09:59:48 +0100

    initial structure cookiecutterV2

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlops_finalproject',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Final project of DTU's course mlops',
+    author='team23',
+    license='',
+)
