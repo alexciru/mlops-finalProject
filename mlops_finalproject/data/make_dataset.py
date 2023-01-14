d48549de0069f41d1b6572c2d1db1be8b07f2d01
michallehwark <michallehwark511@gmail.com>
2023-01-14 17:27:40 +0100

    pulled the changess

--- mlops_finalproject/data/make_dataset.py
+++ mlops_finalproject/data/make_dataset.py
@@ -8,7 +8,6 @@ import os
 import pandas as pd
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-from tqdm import tqdm
 
 
 @click.command()
@@ -22,7 +21,13 @@ def main(input_filepath: str, output_filepath:str):
     logger.info('making final data set from raw data')
 
    # Train files -  organized in folders from labels
-    transform = transforms.Compose([transforms.Resize([30, 30]), transforms.ToTensor()])
+    transform = transforms.Compose([
+    transforms.Resize([224, 224]),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
     train_dataset = datasets.ImageFolder(input_filepath, transform=transform)
 
     # test files -  labels in csv
@@ -32,12 +37,10 @@ def main(input_filepath: str, output_filepath:str):
 
     train_imgs = []
     train_labels = []
-    for it, (img, label) in tqdm(enumerate(train_dataset)):
+    for img, label in train_dataset:
         #flatten_tensor = torch.flatten(img, start_dim=0)
-        train_imgs.append(img)  #flatten_tensor)
+        train_imgs.append(img)
         train_labels.append(label)
-        if it >= 10:    #Testing purpose, only generate 11 images
-            break
 
 
     # # Join all of the data
@@ -73,4 +76,3 @@ if __name__ == '__main__':
     load_dotenv(find_dotenv())
 
     main()
-
